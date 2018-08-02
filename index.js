ec2bc808a1eb753fc7e2086eb382ca9bca3d259c
Thomas Chun <tchun2055@gmail.com>
2018-08-02 23:36:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ function destructivelyAppendKitten(name){
   return kittens.push(name);
 }
 
-function  destructivelyPrependKitten()
+function  destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
 
 function destructivelyRemoveLastKitten(){
-  
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
 }
